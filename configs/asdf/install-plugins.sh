50b35a1727e8b0f11f724cf020cf50f79bf8b1d9
Aerdayne <daenskus@gmail.com>
2021-11-26 23:51:13 +0300

    Migrate to asdf from rbenv, add direnv

---
+++ configs/asdf/install-plugins.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git || true
+asdf plugin-add direnv https://github.com/asdf-community/asdf-direnv || true
--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,1 +1,4 @@
+alias -s be="bundle exec"
+
+direnv hook fish | source
 starship init fish | source
