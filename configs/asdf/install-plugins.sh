2e329b76a770f6807f115fc5e0bf7ab370852501
Aerdayne <daenskus@gmail.com>
2022-01-08 16:05:42 +0300

    asdf nodejs Brewfile: - ansible, DO CLI, chromedriver in - key-codes out

--- configs/asdf/install-plugins.sh
+++ configs/asdf/install-plugins.sh
@@ -2,3 +2,4 @@
 
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git || true
 asdf plugin-add direnv https://github.com/asdf-community/asdf-direnv || true
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git  || true
